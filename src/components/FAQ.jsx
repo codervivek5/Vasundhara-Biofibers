0b7f36ca3de2a5dda9986a4aa15ef89e72b7cd39
Vivek Prajapai <prajapativivek998.com>
2025-03-14 03:32:31 +0530

    make hero section responsive

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -34,13 +34,13 @@ function FAQ() {
                     </div>
                 </div>
             </div>
-            <div class="accordion-item" id="cancel̉-shadow">
-                <button class="accordion-toggle inline-flex items-center gap-x-4 px-5 py-4 text-start" aria-controls="cancel̉-shadow-collapse" aria-expanded="false">
+            <div class="accordion-item" id="cancel-shadow">
+                <button class="accordion-toggle inline-flex items-center gap-x-4 px-5 py-4 text-start" aria-controls="cancel-shadow-collapse" aria-expanded="false">
                     <span class="icon-[tabler--plus] accordion-item-active:hidden text-base-content size-4.5 block shrink-0"></span>
                     <span class="icon-[tabler--minus] accordion-item-active:block text-base-content size-4.5 hidden shrink-0"></span>
                     Can I cancel my order?
                 </button>
-                <div id="cancel̉-shadow-collapse" class="accordion-content hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="cancel̉-shadow" role="region">
+                <div id="cancel-shadow-collapse" class="accordion-content hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="cancel-shadow" role="region">
                     <div class="px-5 pb-4">
                         <p class="text-base-content/80 font-normal">
                             Scheduled delivery orders can be cancelled 72 hours prior to your selected delivery date for full refund.
