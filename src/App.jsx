4424b3908946dbfd9de7c4f0d9d47f1f57f3d607
Vivek Prajapai <prajapativivek998.com>
2025-03-22 18:14:37 +0530

    team page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import FAQ from "./components/FAQ";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ContactUs from "./pages/ContactUs";
-import Team from "./pages/Team";
-import News from "./pages/News";
+import Team from "./pages/team/Team";
+// import News from "./pages/News";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <Route path="faq" element={<FAQ />} />
         <Route path="contact" element={<ContactUs />} />
         <Route path="team" element={<Team />} />
-        <Route path="news" element={<News />} />
+        {/* <Route path="news" element={<News />} /> */}
        
       </Route>
     </Routes>
